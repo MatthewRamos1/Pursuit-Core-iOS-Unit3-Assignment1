c7300efed256a819ae82b061b41767c07c4f236c
Matthew Ramos <mmramos309@gmail.com>
2019-12-09 17:33:53 -0500

    Created function to turn string dates into Date types, began function to create sections

--- PeopleAndAppleStockPricesV2/StockInfo/StockInfo.swift
+++ PeopleAndAppleStockPricesV2/StockInfo/StockInfo.swift
@@ -14,14 +14,27 @@ struct Stock: Decodable {
     let open: Double
     let close: Double
 
-
-static func getStocks (data: Data) -> [Stock] {
-    var stocks = [Stock]()
-    do {
-       stocks = try JSONDecoder().decode([Stock].self, from: data)
-    } catch {
-        fatalError("Can't parse data \(error)")
-    }
-    return stocks
+    static func getStocks (data: Data) -> [Stock] {
+        var stocks = [Stock]()
+        do {
+            stocks = try JSONDecoder().decode([Stock].self, from: data)
+        } catch {
+            fatalError("Can't parse data \(error)")
+        }
+        return stocks
 }
+    static func stringToDate (_ input: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM d, yyyy"
+        guard let newDate = dateFormatter.date(from: input) else {
+            fatalError("Error: Couldn't convert dates")
+        }
+        return newDate
+    }
+
+    static func getSections(_ input: [Stock]) -> [[Stock]] {
+        var sections = [[Stock]]()
+        var sortedStocks = input.sorted { Stock.stringToDate($0.label) < Stock.stringToDate($1.label)}
+    
+    }
 }
--- PeopleAndAppleStockPricesV2/StockInfo/StockInfoListViewController.swift
+++ PeopleAndAppleStockPricesV2/StockInfo/StockInfoListViewController.swift
@@ -12,8 +12,22 @@ class StockInfoListViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var stockInfo = [Stock]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+
+        
+        
+    }
+}
+
+extension StockInfoListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
     }
 }
