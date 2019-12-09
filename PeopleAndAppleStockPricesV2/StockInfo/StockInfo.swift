7b1b6769c21daca3a56963fa50a8a41a7f162c89
Matthew Ramos <mmramos309@gmail.com>
2019-12-09 16:39:56 -0500

    Added stock object type to parse to and parsing function

--- PeopleAndAppleStockPricesV2/StockInfo/StockInfo.swift
+++ PeopleAndAppleStockPricesV2/StockInfo/StockInfo.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+struct Stock: Decodable {
+    let date: String
+    let label: String
+    let open: Double
+    let close: Double
+
+
+static func getStocks (data: Data) -> [Stock] {
+    var stocks = [Stock]()
+    do {
+       stocks = try JSONDecoder().decode([Stock].self, from: data)
+    } catch {
+        fatalError("Can't parse data \(error)")
+    }
+    return stocks
+}
+}
--- PeopleAndAppleStockPricesV2/StockInfo/StockInfoListViewController.swift
+++ PeopleAndAppleStockPricesV2/StockInfo/StockInfoListViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class StockInfoListViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
